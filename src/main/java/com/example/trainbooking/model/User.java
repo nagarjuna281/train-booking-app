09dd0c51b60612053c453b0ab144abcb4f2c0795
nagarjuna281 <kamalenagarjuna@gmail.com>
2025-09-16 13:58:55 +0530

    Create User.java

---
+++ src/main/java/com/example/trainbooking/model/User.java
@@ -0,0 +1,15 @@
+package com.example.trainbooking.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
+    private String password;
+}
