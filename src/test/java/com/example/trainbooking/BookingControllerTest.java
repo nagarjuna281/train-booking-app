9936277f4d7ed814346975b72f572c2119653345
nagarjuna281 <kamalenagarjuna@gmail.com>
2025-09-16 16:07:12 +0530

    Update BookingControllerTest.java

--- src/test/java/com/example/trainbooking/BookingControllerTest.java
+++ src/test/java/com/example/trainbooking/BookingControllerTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTest(properties = {"spring.sql.init.mode=never"})  // Disable data.sql init in tests to avoid errors
 @AutoConfigureMockMvc
 class BookingControllerTest {
 
