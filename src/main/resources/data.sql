f13c32ad811934d62d0fcdc233c474aeea1d84f4
nagarjuna281 <kamalenagarjuna@gmail.com>
2025-09-16 14:03:30 +0530

    Create data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO train (route, departure_time, available_seats) VALUES ('NYC to Boston', '2023-10-01T10:00:00', 100);
+INSERT INTO train (route, departure_time, available_seats) VALUES ('Boston to NYC', '2023-10-02T14:00:00', 50);
